d36a06e4ebb47b3ba797f19b1167fe793813562b
Vinícius Pavani <viniciusalvespavani@hotmail.com>
2022-02-25 17:35:29 -0300

    style: estilizacao do footer

--- src/layout/FooterMr.vue
+++ src/layout/FooterMr.vue
@@ -1,17 +1,16 @@
 <template>
-  <footer class="footer-background mt-5 pt-3 ">
-    <div class="d-flex justify-content-around">
+  <footer class="footer-background mt-5 pt-3">
+    <div class="footer-border d-flex justify-content-around pl-3">
       <section class="footer-section">
-        <h3 class="text-center section-title">Logo</h3>
-        <div class="section-content d-flex flex-column justify-content-center">
-          <p>Mainoralho é um projeto criado para o planejamento das sprints, tendo função semelhante a de um planning poker</p>
-          <div>redes sociais</div>
+        <img class="logo" src="../assets/logo_maino_branco.png" alt="Logo da empresa maino">
+        <div class="pt-3 d-flex align-items-center">
+          <p>Mainoralho é um projeto criado para o planejamento das sprints, tendo baseado na tecnica de planejamento planning poker</p>
         </div>
       </section>
       
       <section class="footer-section">
-        <h3 class="text-center section-title">Contato</h3>
-        <div class="section-content d-flex justify-content-center align-items-center">
+        <h3 class="section-title">Contato</h3>
+        <div class="pt-3 d-flex align-items-center">
           <div>
             <div>
               <span class="mr-1">Telefone: </span>
@@ -22,16 +21,27 @@
               <span class="mr-1">Email: </span>
               <span>maino.contato@maino.com.br</span>
             </div>
+
+            <div class="d-flex pt-3">
+              <a href="https://www.instagram.com/mainosistemas/"><i class="fab fa-instagram mr-3"></i></a>
+              <a href="https://www.facebook.com/mainosistemas/"><i class="fab fa-facebook-square mr-3"></i></a>
+              <a href="https://www.linkedin.com/company/mainosistemas"><i class="fab fa-linkedin"></i></a>
+            </div>
           </div>
         </div>
       </section>
 
       <section class="footer-section">
-        <h3 class="text-center section-title">Onde nos encontrar</h3>
-        <p></p>
+        <h3 class="section-title">Onde nos encontrar</h3>
+        <div class="pt-3 d-flex flex-column justify-content-center">
+          <p class="m-0">Av. Pastor Martin Luther King Júnior, 126</p>
+          <p class="m-0">Del Castilho, Rio de Janeiro - RJ</p>
+          <p class="m-0">CEP: 20760-005</p>
+          <p class="m-0">Nova America Offices 3000, sala 812</p>
+        </div>
       </section>
     </div>
-    <p class="text-center pt-3 mb-0 pb-2"> &copy; Copyright Mainoralho - 2022 </p>
+    <p class="copyright text-center pt-2 mb-0 pb-2"> &copy; Copyright Mainoralho - 2022 </p>
   </footer>
 </template>
 
@@ -43,9 +53,9 @@ export default {
 
 <style lang="scss">
   @import '../assets/sass/variables';
-
   .footer-background {
     background: $maino_blue;
+    border-radius: 35px 35px 0 0;
     color: #f5f5f5;
   }
 
@@ -54,13 +64,23 @@ export default {
   }
 
   .section-title {
+    color: $maino_green;
     height: 15%;
     padding-bottom: 2rem;
   }
 
-  .section-content {
-    height: 80%;
+  .logo {
+    height: 80px;
+    width: auto;
   }
 
+  .copyright{
+    background: $maino_blue_hover;
+  }
 
+  .fab {
+    color: $maino_green;
+  }.fab:hover {
+    color: $maino_green_hover;
+  }
 </style>
